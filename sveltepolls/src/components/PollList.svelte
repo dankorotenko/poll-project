2ac9ebf17e6aed8775960c1ee9f50b62907c4359
Данил Коротенко <dankorotenko@mbpro.local>
2021-05-10 18:55:32 +0200

    initial commit

---
+++ sveltepolls/src/components/PollList.svelte
@@ -0,0 +1,41 @@
+<script>
+
+// import { onDestroy, onMount } from 'svelte';
+import { fade, scale, slide } from 'svelte/transition';
+import { flip } from 'svelte/animate';
+import PollStore from '../stores/PollStore.js';
+import PollDetails from "./PollDetails.svelte";
+
+
+
+    // export let polls = [];
+
+    // const unsub = PollStore.subscribe( data => {
+    //     polls = data;
+    // });
+
+    // onDestroy( () => {
+    //     unsub();
+    // });
+
+
+
+</script>
+
+<div class="poll-list">
+{#each $PollStore as poll (poll.id)}
+    <div in:fade out:scale|local animate:flip={{duration: 500}}>
+        <PollDetails {poll} />
+    </div>
+{/each}
+</div>
+
+
+
+<style>
+    .poll-list{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+    }
+</style>
